835e47d8c066319e097f2f860e131af25c098472
mrsinham <julien.lefevr@gmail.com>
2026-01-26 10:48:17 +0100

    fix: use stricter file permissions for config files

--- cmd/dicomforge/wizard/config.go
+++ cmd/dicomforge/wizard/config.go
@@ -38,7 +38,7 @@ func SaveToYAML(state *WizardState, path string) error {
 		return fmt.Errorf("marshaling config: %w", err)
 	}
 
-	if err := os.WriteFile(path, data, 0644); err != nil {
+	if err := os.WriteFile(path, data, 0600); err != nil {
 		return fmt.Errorf("writing config file: %w", err)
 	}
 
